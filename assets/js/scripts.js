3f9094af5bcbbf708db124e327fb518324592638
Marcos Leite <m.abner@live.co.uk>
2022-01-31 22:56:25 -0300

    adds validation do disable - and + buttons when number reacher -10 and 10

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,12 +6,14 @@ function increment() {
 	currentNumber = currentNumber + 1;
 	currentNumberWrapper.innerHTML = currentNumber;
 	changeTextColour();
+	disableButtons();
 }
 
 function decrement() {
 	currentNumber = currentNumber - 1;
 	currentNumberWrapper.innerHTML = currentNumber;
 	changeTextColour();
+	disableButtons();
 }
 
 function changeTextColour() {
@@ -19,5 +21,16 @@ function changeTextColour() {
 		currentNumberWrapper.style.color = "red";
 	} else {
 		currentNumberWrapper.style.color = "black";
+	}
+}
+
+function disableButtons() {
+	if (currentNumber == -10) {
+		document.getElementById("decrement").disabled = true;
+	} else if (currentNumber == 10) {
+		document.getElementById("increment").disabled = true;
+	} else {
+		document.getElementById("decrement").disabled = false;
+		document.getElementById("increment").disabled = false;
 	}
 }
